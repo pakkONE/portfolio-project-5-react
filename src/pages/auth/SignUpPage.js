1a94430f8ed53de54d2187cda67420cb7dd2e683
pakkONE <101711895+pakkONE@users.noreply.github.com>
2022-07-26 23:03:33 +0200

    Create NavBar with placeholder pages signin/up

--- src/pages/auth/SignUpPage.js
+++ src/pages/auth/SignUpPage.js
@@ -1,13 +1,29 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import styles from '../../styles/SignUpPage.module.css'
 
 const SignUpPage = () => {
     return (
         <div>
-            <hr />
-            <hr />
-            <hr />
-            <hr />
-            SignUpPage
+            <Form>
+                <Form.Group className={styles.Form} controlId="formBasicEmail">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control className={styles.FormControl} type="email" placeholder="Enter username" />
+                </Form.Group>
+
+                <Form.Group className={styles.Form} controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control className={styles.FormControl} type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className={styles.Form} controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control className={styles.FormControl} type="password" placeholder="Confirm password" />
+                </Form.Group>
+                <Button className={styles.Button} type="submit">
+                    Submit
+                </Button>
+            </Form>
         </div>
     )
 }
