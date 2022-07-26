cf350b7708d66427ec2ce500ae5655f9e554e566
pakkONE <101711895+pakkONE@users.noreply.github.com>
2022-07-26 12:28:27 +0200

    Added favicon and name to website

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { Button } from "react-bootstrap";
-import "./App.css";
+import styles from './App.module.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <Button variant="primary">Testing that Bootstrap and Font Awesome Icons works! <i className="fas fa-heart" /></Button>
     </div>
   );
