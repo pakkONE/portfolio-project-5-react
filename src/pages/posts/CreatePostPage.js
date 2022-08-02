399d8fc44497e0b6e1b32f5fe3d86895a8091542
pakkONE <101711895+pakkONE@users.noreply.github.com>
2022-08-02 02:25:28 +0200

    removed container and refactored code for react 18

--- src/pages/posts/CreatePostPage.js
+++ src/pages/posts/CreatePostPage.js
@@ -72,107 +72,101 @@ const CreatePostPage = () => {
   return (
     <div>
       <Form className="pb-2" onSubmit={handleSubmit}>
-        <Container fluid className="p-3">
-          <Row className="justify-content-center">
-            <Col
-              md={10}
-              className={`p-3 my-3 bg-light rounded-4 ${styles.Upload}`}
-            >
-              <Container className="m-1 py-2 px-5 text-center">
-                <Form.Group className="my-3">
-                  {image ? (
-                    <>
-                      <div>
-                        <Image
-                          className={styles.UploadedImage}
-                          src={image}
-                          fluid
-                          rounded
-                        />
-                      </div>
-                      <div>
-                        <Form.Label
-                          htmlFor="image-upload"
-                          className={`${styles.Btn28} mt-3`}
-                        >
-                          Change image
-                        </Form.Label>
-                      </div>
-                    </>
-                  ) : (
-                    <>
-                      <h3>Click on the icon below to upload an image</h3>
-                      <Form.Label className="my-3" htmlFor="image-upload">
-                        <i
-                          className={`${styles.Icon} fa-solid fa-cloud-arrow-up`}
-                        ></i>
+        <Row className="justify-content-center">
+          <Col
+            md={10}
+            className={`p-3 my-3 bg-light rounded-4 ${styles.Upload}`}
+          >
+            <Container className="m-1 py-2 px-5 text-center">
+              <Form.Group className="my-3">
+                {image ? (
+                  <>
+                    <div>
+                      <Image
+                        className={styles.UploadedImage}
+                        src={image}
+                        rounded
+                      />
+                    </div>
+                    <div>
+                      <Form.Label
+                        htmlFor="image-upload"
+                        className={`${styles.Btn28} mt-3`}
+                      >
+                        Change image
                       </Form.Label>
-                    </>
-                  )}
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <h3>Click on the icon below to upload an image</h3>
+                    <Form.Label className="my-3" htmlFor="image-upload">
+                      <i
+                        className={`${styles.Icon} fa-solid fa-cloud-arrow-up`}
+                      ></i>
+                    </Form.Label>
+                  </>
+                )}
 
+                <Form.Control
+                  id="image-upload"
+                  type="file"
+                  name="image"
+                  hidden
+                  accept="image/*"
+                  onChange={handleChangeImage}
+                  ref={imageInput}
+                />
+              </Form.Group>
+
+              <div className="my-2 p-3">
+                <Form.Group>
+                  <Form.Label>Title</Form.Label>
                   <Form.Control
-                    id="image-upload"
-                    type="file"
-                    name="image"
-                    hidden
-                    accept="image/*"
-                    onChange={handleChangeImage}
-                    ref={imageInput}
+                    type="text"
+                    name="title"
+                    value={title}
+                    onChange={handleChange}
                   />
                 </Form.Group>
 
-                <div className="my-2 p-3">
-                  <Form.Group>
-                    <Form.Label>Title</Form.Label>
-                    <Form.Control
-                      type="text"
-                      name="title"
-                      value={title}
-                      onChange={handleChange}
-                    />
-                  </Form.Group>
+                <Form.Group className="my-2">
+                  <Form.Label>Content</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    rows={6}
+                    name="content"
+                    value={content}
+                    onChange={handleChange}
+                  />
+                </Form.Group>
 
-                  <Form.Group className="my-2">
-                    <Form.Label>Content</Form.Label>
-                    <Form.Control
-                      as="textarea"
-                      rows={6}
-                      name="content"
-                      value={content}
-                      onChange={handleChange}
-                    />
-                  </Form.Group>
+                <Form.Group className="my-2">
+                  <Form.Label>Tags</Form.Label>
+                  <Form.Control onChange={handleTags} name="tags">
+                    <option>Select a tag</option>
+                    <option value="FO">Football</option>
+                    <option value="IH">Ice Hockey</option>
+                    <option value="GO">Golf</option>
+                    <option value="TE">Tennis</option>
+                    <option value="PA">Padel</option>
+                    <option value="OT">Other</option>
+                  </Form.Control>
+                </Form.Group>
 
-                  <Form.Group className="my-2">
-                    <Form.Label>Tags</Form.Label>
-                    <Form.Select
-                      aria-label="select a sport the post is about"
-                      onChange={handleTags}
-                    >
-                      <option>Select a tag</option>
-                      <option value="FO">Football</option>
-                      <option value="IH">Ice Hockey</option>
-                      <option value="GO">Golf</option>
-                      <option value="TE">Tennis</option>
-                      <option value="PA">Padel</option>
-                      <option value="OT">Other</option>
-                    </Form.Select>
-                  </Form.Group>
-
-                  <Button
-                    className={`${styles.Btn28} mt-3`}
-                    onClick={() => navigate(-1)}
-                  >
-                    Cancel
-                  </Button>
-                  <Button className={`${styles.Btn28} mt-3`} type="submit">
-                    Create
-                  </Button>
-                </div>
-              </Container>
-            </Col>
-          </Row>
-        </Container>
+                <Button
+                  className={`${styles.Btn28} mt-3`}
+                  onClick={() => navigate(-1)}
+                >
+                  Cancel
+                </Button>
+                <Button className={`${styles.Btn28} mt-3`} type="submit">
+                  Create
+                </Button>
+              </div>
+            </Container>
+          </Col>
+        </Row>
       </Form>
     </div>
   );
